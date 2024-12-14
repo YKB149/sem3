9030e59e8c6f263c801ad9ab24304b681616813d
YKB149 <81851279+YKB149@users.noreply.github.com>
2024-12-14 22:14:53 +0530

    Update Exper10.cpp

--- Exper10.cpp
+++ Exper10.cpp
@@ -1,4 +1,17 @@
+/*AIM:
+Implement C++ program for expression conversion 
+as infix to postfix 
+and its evaluation using stack based on given conditions:
+1. Operands and operator, both must be single character.
+2. Input Postfix expression must be in a desired format.
+3. Only '+', '-', '*' and '/' operators are expected
 
+INPUT: 
+    1. Infix Expression
+	
+OUTPUT:
+	1) Display postfix expression
+	*/
 #include<iostream>
 #include<string.h>
 #define MAX 10
