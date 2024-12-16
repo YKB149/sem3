91e7ea1275152cb325a46d3e0ddbc2003394c992
YKB149 <81851279+YKB149@users.noreply.github.com>
2024-12-16 09:29:09 +0530

    Add files via upload

---
+++ oop/oop4.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+class student
+{
+  string name;
+  int roll_no;
+  char division;
+  
+  public:
+  void getdata()
+  {
+    ofstream out ("stud",ios::app);
+    for(int i=0;i<3;i++)
+    {
+      cout<<"Enter Name\n";
+      cin>>name;
+      cout<<"Enter Roll No\n";
+      cin>>roll_no;
+      cout<<"Enter Division\n";
+      cin>>division;
+      out<<name<<"\t"<<roll_no<<"\t"<<division<<"\n";
+    }
+    out.close();
+  }
+  
+  void display()
+  {
+    char line[20];
+    ifstream in("stud");
+    while(in)
+    {
+      in.getline(line,20);
+      cout<<line<<endl;
+    }
+    for(int i=0;i<3;i++)
+    {
+      in>>name>>roll_no>>division;
+      cout<<name<<"\t"<<roll_no<<"\t"<<division<<"\n";
+    }
+    in.close();
+  }
+};
+
+int main()
+{
+  student s1;
+  s1.getdata();
+  s1.display();
+  
+  return 0;
+}
